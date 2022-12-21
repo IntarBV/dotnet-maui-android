a06e819b161b587afdddb2525a05dfefe09fb676
Gijsbert ter Horst <gijsbert.terhorst@intar.nl>
2022-12-21 13:52:00 +0100

    Add getopts and option to push after build

--- build.sh
+++ build.sh
@@ -1,5 +1,33 @@
+#!/bin/bash
 source defaults.sh
+function usage {
+  cat << EOF
+Usage: $0 [-h] -p
+OPTIONS:
+  -h        Show this message
+  -p        Push after build
+ERROR CODES:
+  1         Invalid option
+EOF
+}
 
+optstring=":h:p"
+unset push
+
+while getopts ${optstring} arg; do
+  case ${arg} in
+    h)
+      usage
+      ;;
+    p)
+      push=Yes
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 1
+      ;;
+  esac
+done
 echo "Maintainer:           ${MAINTAINER}"
 echo "Docker hub username:  ${DOCKER_HUB_USERNAME}"
 echo "Time zone:            ${TZ}"
@@ -8,6 +36,7 @@ echo "Java version:         ${JAVA_VERSION}"
 echo "Android API:          ${ANDROID_API}"
 echo "Build tools version:  ${BUILD_TOOLS_VERSION}"
 echo "Extra tags:           ${EXTRA_TAGS[@]}"
+echo "Push after build:     ${push}"
 
 COMMON_BUILD_ARGS=(
   "--build-arg" "MAINTAINER=${MAINTAINER}"
@@ -27,3 +56,11 @@ do
   echo "Tagging ${DOCKER_HUB_USERNAME}/dotnet-maui-android:latest as ${DOCKER_HUB_USERNAME}/dotnet-maui-android:${tag}"
   docker tag "${DOCKER_HUB_USERNAME}/dotnet-maui-android:latest" "${DOCKER_HUB_USERNAME}/dotnet-maui-android:${tag}"
 done
+
+if [ -n "${push}" ]
+then
+  for tag in latest ${DOTNET_VERSION} ${EXTRA_TAGS[@]}
+  do
+    docker push "${DOCKER_HUB_USERNAME}/dotnet-maui-android:${tag}"
+  done
+fi
