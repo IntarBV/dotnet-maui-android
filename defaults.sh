0554fd90c660740d5a21ae8918f910dd709c4f38
Gijsbert ter Horst <gijsbert.terhorst@intar.nl>
2024-03-14 09:31:34 +0100

    Auto calculate `ANDROID_API` and `BUILD_TOOLS_VERSION`:
    They're mostly determined by MAUI version anyway.

--- defaults.sh
+++ defaults.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-# API levels available and which build tools version corresponds to it.
-declare -A BUILD_TOOLS=( [31]="31.0.0" [32]="32.0.0" [33]="33.0.2" [34]="34.0.0" )
-
 # Use a .env file to easily override values in your fork
-if [[ -f .env ]]
-then
+if [[ -f .env ]]; then
   source .env
 fi
 
-if [[ -z $EXTRA_TAGS ]]
-then
+if [[ -z $EXTRA_TAGS ]]; then
   EXTRA_TAGS=()
 fi
 
-if [[ $TAG_WITH_DATE = 1 || $TAG_WITH_DATE = 'yes' || $1 = '--tag-with-date' ]]
-then
+if [[ $TAG_WITH_DATE = 1 || $TAG_WITH_DATE = 'yes' || $1 = '--tag-with-date' ]]; then
   EXTRA_TAGS+=($(date +%Y%m%d))
 fi
 
@@ -24,9 +18,9 @@ DOCKER_HUB_USERNAME=${DOCKER_HUB_USERNAME:-"conneqthub"}
 TZ=${TZ:-"UTC"}
 
 # You must have the most recent maui-android workload installed for this to work.
-# The .NET‌ CLI doesn't show version numbers of workloads that are not already installed. 
-MAUI_VERSION=${MAUI_VERSION:-"$(dotnet workload list | sed -En 's|^maui-android +([^/]+)/.+$|\1|p')"}
+# The .NET‌ CLI doesn't show version numbers of workloads that are not already installed.
+MAUI_VERSION=${MAUI_VERSION:-"$(dotnet workload list | sed -En 's|^maui-android\s+([^/]+)/.+$|\1|p')"}
 DOTNET_VERSION=${DOTNET_VERSION:-"8.0"}
 JAVA_VERSION=${JAVA_VERSION:-"17"}
-ANDROID_API=${ANDROID_API:-"34"}
-BUILD_TOOLS_VERSION=${BUILD_TOOLS[${ANDROID_API}]}
+ANDROID_API=${ANDROID_API:-"$(dotnet workload list | sed -En 's|^android\s+([0-9]+).+$|\1|p')"}
+BUILD_TOOLS_VERSION=${BUILD_TOOLS_VERSION:-$(sdkmanager --list 2>/dev/null | grep -E "build-tools;(${ANDROID_API}\.[0-9]+\.[0-9]+)\s+.+$" | tail -1 | sed -En 's|^.+build-tools;(\S+).+$|\1|p')}
